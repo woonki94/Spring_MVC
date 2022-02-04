0a38996a6e884136418a4f0bc1da65df74f2eb8f
woonki94 <woonki94@naver.com>
2022-02-04 16:31:48 +0900

    회원관리 web-app.v3(Model 추가)

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -17,7 +18,16 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
-
-
     }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request){
+        model.forEach((key, value)->request.setAttribute(key,value));
+    }
+
 }
