5972f42a0a6230b7c1717367f6c9a96593c1f24c
woonki94 <woonki94@naver.com>
2022-02-06 15:32:38 +0900

    SpringMVC

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.springmvc.v1;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+/**
+ * 아래처럼바꿔도 실행됨
+ */
+//@Component
+//@RequestMapping
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process(){
+        return new ModelAndView("new-form");
+    }
+}
